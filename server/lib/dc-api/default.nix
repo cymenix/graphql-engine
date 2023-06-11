8b2284abcfa7e5c0b49f4405e40d5afb5317045e
John Rodewald <jnrodewald99@gmail.com>
2023-06-11 16:55:49 +0200

    Fix `dc-api` deps

--- server/lib/dc-api/default.nix
+++ server/lib/dc-api/default.nix
@@ -1,12 +1,12 @@
 { mkDerivation, aeson, ansi-terminal, autodocodec
 , autodocodec-openapi3, base, bytestring, case-insensitive
 , data-fix, deepseq, Diff, directory, extra, file-embed, filepath
-, ghc-heap-view, graphql-parser, hashable, hspec, hspec-core
+, ghc-heap-view, graphql-parser, hashable, hasura-extras, hspec, hspec-core
 , http-client, http-media, insert-ordered-containers, lens
 , lens-aeson, lib, mtl, network-uri, openapi3, optparse-applicative
 , scientific, servant, servant-client, servant-client-core
 , servant-openapi3, text, time, unordered-containers, vector
-, xml-conduit, xml-lens, yaml, zlib
+, witch, xml-conduit, xml-lens, yaml, zlib
 }:
 mkDerivation {
   pname = "dc-api";
@@ -14,10 +14,10 @@ mkDerivation {
   src = ./.;
   libraryHaskellDepends = [
     aeson autodocodec autodocodec-openapi3 base bytestring deepseq
-    extra ghc-heap-view graphql-parser hashable http-media
+    extra ghc-heap-view graphql-parser hashable hasura-extras http-media
     insert-ordered-containers lens lens-aeson openapi3 scientific
     servant servant-client servant-client-core servant-openapi3 text
-    unordered-containers
+    unordered-containers witch
   ];
   testHaskellDepends = [
     aeson ansi-terminal autodocodec autodocodec-openapi3 base
