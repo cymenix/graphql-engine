85987d899a88b09032401e6718837742d0043163
John Rodewald <jnrodewald99@gmail.com>
2023-06-12 13:04:28 +0200

    Reorganise overlay

--- nix/overlays/packages.nix
+++ nix/overlays/packages.nix
@@ -3,34 +3,38 @@ self: super: {
     packages = super.haskell.packages // {
       ${super.ghcName} = super.haskell.packages.${super.ghcName}.override {
         overrides = hself: hsuper: {
+          # Hasura packages
           # NOTE: avoiding IFD's/automatically generated derivations in favor of cabal2nix generated default.nix
           aeson-ordered = hsuper.callPackage ../../server/lib/aeson-ordered { };
           api-tests = hsuper.callPackage ../../server/lib/api-tests { };
-
-          # TODO: some typed hole errors ATM + tests don't compile as extra-source-files are missing
-          dc-api = super.haskell.lib.dontCheck (hsuper.callPackage ../../server/lib/dc-api { 
-            hasura-extras = hself.hasura-extras;
-          });
           hasura-error-message =
             hsuper.callPackage ../../server/lib/error-message { };
-          graphql-parser =
-            hsuper.callPackage ../../server/lib/graphql-parser {
-              hedgehog = hself.hedgehog_1_2;
-            };
+          graphql-parser = hsuper.callPackage ../../server/lib/graphql-parser {
+            hedgehog = hself.hedgehog_1_2;
+          };
           hasura-prelude =
             hsuper.callPackage ../../server/lib/hasura-prelude { };
+          test-harness = hsuper.callPackage ../../server/lib/test-harness { };
+          hasura-extras = hsuper.callPackage ../../server/lib/hasura-extras { };
+          hasura-base = hsuper.callPackage ../../server/lib/hasura-base { };
+          hasura-json-encoding =
+            hsuper.callPackage ../../server/lib/hasura-json-encoding { };
+          arrows-extra = hsuper.callPackage ../../server/lib/arrows-extra { };
+
+          # Hasura packages (tests skipped)
+          hasura-schema-parsers = super.haskell.lib.dontCheck
+            (hsuper.callPackage ../../server/lib/schema-parsers { });
           pg-client = super.haskell.lib.dontCheck
             (hsuper.callPackage ../../server/lib/pg-client {
               resource-pool = hself.hasura-resource-pool;
             });
+          # TODO: some typed hole errors ATM + tests don't compile as extra-source-files are missing
+          dc-api = super.haskell.lib.dontCheck
+            (hsuper.callPackage ../../server/lib/dc-api {
+              hasura-extras = hself.hasura-extras;
+            });
 
-          hasura-schema-parsers = super.haskell.lib.dontCheck (hsuper.callPackage ../../server/lib/schema-parsers { });
-          test-harness = hsuper.callPackage ../../server/lib/test-harness { };
-          hasura-extras = hsuper.callPackage ../../server/lib/hasura-extras { };
-          hasura-base = hsuper.callPackage ../../server/lib/hasura-base { };
-          hasura-json-encoding = hsuper.callPackage ../../server/lib/hasura-json-encoding { };
-
-          arrows-extra = hsuper.callPackage ../../server/lib/arrows-extra { };
+          # Remote sources
 
           graphql-server = super.haskell.lib.justStaticExecutables
             ((super.haskell.lib.dontCheck (hsuper.callPackage ../../server {
@@ -51,16 +55,17 @@ self: super: {
 
           # https://gutier.io/post/development-fixing-broken-haskell-packages-nixpkgs/
           ekg-prometheus = super.haskell.lib.doJailbreak
-            (hsuper.callCabal2nix "ekg-prometheus" (super.fetchFromGitHub {
-              owner = "hasura";
-              repo = "ekg-prometheus";
-              rev = "131bd4fe72764377d2dae098df0c0e35e3b09982";
-              sha256 = "15EqVL+5AWz3588Ilb9dAgb+i9LaUhBEXHZ/f8jp8/4=";
-            }) { });
+            (super.haskell.lib.dontCheck (hsuper.callCabal2nix "ekg-prometheus"
+              (super.fetchFromGitHub {
+                owner = "hasura";
+                repo = "ekg-prometheus";
+                rev = "1bb4657e5eb8200560bb9a4af933523d60bcd587";
+                sha256 = "0r9cdzss380i4s67hv8cahw3mms3vh842q7wd5sxj420z1cprcv2";
+              }) { }));
 
           # FIXME: reenable tests that fail upstream
-          kriti-lang = super.haskell.lib.dontCheck (hsuper.callCabal2nix "kriti-lang"
-            (super.fetchFromGitHub {
+          kriti-lang = super.haskell.lib.dontCheck
+            (hsuper.callCabal2nix "kriti-lang" (super.fetchFromGitHub {
               owner = "hasura";
               repo = "kriti-lang";
               rev = "daf56edd514a3c5439b457f9de08eaf43c876251";
@@ -98,14 +103,6 @@ self: super: {
               sha256 = "0alvd156k0n0x1i3apgznjqrvkawga1dzh21px3zk3vs690hrlsp";
             }) { });
 
-          # broken dependency fixe
-          # openapi3-3.2.2: lens >=4.16.1 && <5.2
-          openapi3 = super.haskell.lib.doJailbreak hsuper.openapi3;
-          servant-openapi3 =
-            super.haskell.lib.doJailbreak hsuper.servant-openapi3;
-          ghc-heap-view =
-            super.haskell.lib.disableLibraryProfiling hsuper.ghc-heap-view;
-
           th-extras = hsuper.callCabal2nix "th-extras" (super.fetchFromGitHub {
             owner = "erikd";
             repo = "th-extras";
@@ -113,21 +110,24 @@ self: super: {
             sha256 = "0abpki0y8y4cavv3k1sbqa3jc62rjr8bj7l8j313kjhn6v4hhg1c";
           }) { };
 
-          ghc-debug-stub = hsuper.callCabal2nixWithOptions "ghc-debug-stub" (super.fetchFromGitLab {
-            domain = "gitlab.haskell.org";
-            owner = "ghc";
-            repo = "ghc-debug";
-            rev = "6671279c2b4c874291ea67ff67c39f24e1642109";
-            sha256 = "18q9q694rqmj9nv718pwqlkh3ak4g1ra90nlb3k9hw34xbkcl3wf";
-          }) "--subpath stub" { };
+          ghc-debug-stub = hsuper.callCabal2nixWithOptions "ghc-debug-stub"
+            (super.fetchFromGitLab {
+              domain = "gitlab.haskell.org";
+              owner = "ghc";
+              repo = "ghc-debug";
+              rev = "6671279c2b4c874291ea67ff67c39f24e1642109";
+              sha256 = "18q9q694rqmj9nv718pwqlkh3ak4g1ra90nlb3k9hw34xbkcl3wf";
+            }) "--subpath stub" { };
 
-          ghc-debug-convention = hsuper.callCabal2nixWithOptions "ghc-debug-convention" (super.fetchFromGitLab {
-            domain = "gitlab.haskell.org";
-            owner = "ghc";
-            repo = "ghc-debug";
-            rev = "6671279c2b4c874291ea67ff67c39f24e1642109";
-            sha256 = "18q9q694rqmj9nv718pwqlkh3ak4g1ra90nlb3k9hw34xbkcl3wf";
-          }) "--subpath convention" { };
+          ghc-debug-convention =
+            hsuper.callCabal2nixWithOptions "ghc-debug-convention"
+            (super.fetchFromGitLab {
+              domain = "gitlab.haskell.org";
+              owner = "ghc";
+              repo = "ghc-debug";
+              rev = "6671279c2b4c874291ea67ff67c39f24e1642109";
+              sha256 = "18q9q694rqmj9nv718pwqlkh3ak4g1ra90nlb3k9hw34xbkcl3wf";
+            }) "--subpath convention" { };
 
           # ghc-debug-stub = hsuper.ghc-debug-stub.override {
           #   overrides = hself: hsuper: {
@@ -135,6 +135,14 @@ self: super: {
           #     sha256 = "18q9q694rqmj9nv718pwqlkh3ak4g1ra90nlb3k9hw34xbkcl3wf";
           #   };
           # };
+
+          # Other broken dependencies
+          # openapi3-3.2.2: lens >=4.16.1 && <5.2
+          openapi3 = super.haskell.lib.doJailbreak hsuper.openapi3;
+          servant-openapi3 =
+            super.haskell.lib.doJailbreak hsuper.servant-openapi3;
+          ghc-heap-view =
+            super.haskell.lib.disableLibraryProfiling hsuper.ghc-heap-view;
         };
       };
     };
